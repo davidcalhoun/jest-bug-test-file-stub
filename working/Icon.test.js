5f2e5078e1b6c5955fb1e22ab88e9efa8032d865
David Calhoun <dcalhoun@methanesat.org>
2020-11-18 09:37:04 -0500

    add tests with ReactDOM rendering

--- working/Icon.test.js
+++ working/Icon.test.js
@@ -1,8 +1,18 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import { render } from "@testing-library/react";
 import Icon from "./Icon";
 
-test("has a className", () => {
-  const { container } = render(<Icon />);
-  expect(container.firstChild.className).toBe("myLogo");
+describe("has a className", () => {
+  test("ReactDOM rendering", () => {
+    const container = document.createElement("div");
+    ReactDOM.render(<Icon />, container);
+    expect(container.firstChild.className).toBe("myLogo");
+  });
+
+  test("react testing library rendering", () => {
+    const { container } = render(<Icon />);
+    render(<Icon />);
+    expect(container.firstChild.className).toBe("myLogo");
+  });
 });
